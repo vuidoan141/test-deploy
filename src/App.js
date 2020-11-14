ce3115b82d9e5b850d74965106ca07dbe08f6472
Vui Doan <zjnka141@gmail.com>
2020-11-14 22:45:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState({})
+  axios.get('https://shrouded-tundra-48868.herokuapp.com/users').then(resp => {
+    setUser(resp.data)
+  })
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{`${user.name} - ${user.age}`}</p>
       </header>
     </div>
   );
